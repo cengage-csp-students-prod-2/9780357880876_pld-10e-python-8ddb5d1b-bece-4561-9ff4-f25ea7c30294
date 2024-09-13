7ff3d46a0fed5d4b8dcc9e7c7256c887226cd86f
tahasalayma <taha.salayma@gmail.com>
2024-09-13 22:17:49 +0000

    Testing Fri Sep 13 2024 22:17:49 GMT+0000 (Coordinated Universal Time)

--- chapter0/ex00/student/example.py
+++ chapter0/ex00/student/example.py
@@ -1,10 +1,7 @@
 #This is the code editor
 def example():
-    print("This is the code editor.")
+    print("This is a simple activity.")
 
 if __name__ == "__main__":
     example()
-    def example():
-    print("This is a simple activity.")
-if __name__ == "__main__":
-    example()
+    
