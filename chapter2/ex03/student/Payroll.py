53b1286be95b7c6f6483a97ca868a03ba3f8089c
tahasalayma <taha.salayma@gmail.com>
2024-10-14 21:48:01 +0000

    Testing Mon Oct 14 2024 21:48:01 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex03/student/Payroll.py
+++ chapter2/ex03/student/Payroll.py
@@ -1,24 +1,22 @@
 # This program calculates an employee's take home pay.
 
-
 salary = 1250.00
 numDependents = 2
 
-# Calculate state tax here.
+# Calculate state tax here
+stateTax = 0.05 * salary
 
 print(f"State Tax: ${stateTax:.2f}")
 
-# Calculate federal tax here.
+# Calculate federal tax here
+federalTax = 0.10 * salary
 
 print(f"Federal Tax: ${federalTax:.2f}")
 
-# Calculate dependant deduction here.
+# Calculate dependent deduction here
+dependentDeduction = 100 * numDependents
 
-print(f"Dependents: ${dependentDeduction:.2f}")
-
-# Calculate total withholding here.
-
-print(f"Total withholding ${totalWithholding:.2f}")
+print(f"Dependents Deduction: ${dependentDeduction:.2f}")
 
 
 # Calculate take home pay here.
