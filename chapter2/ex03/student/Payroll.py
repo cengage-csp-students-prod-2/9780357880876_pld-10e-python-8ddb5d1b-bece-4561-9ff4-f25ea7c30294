56554ae7a66d86d664da512f817ba663780d5c94
tahasalayma <taha.salayma@gmail.com>
2024-10-14 21:53:20 +0000

    Testing Mon Oct 14 2024 21:53:20 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex03/student/Payroll.py
+++ chapter2/ex03/student/Payroll.py
@@ -4,22 +4,20 @@ salary = 1250.00
 numDependents = 2
 
 # Calculate state tax here
-stateTax = 0.05 * salary
-
+stateTax = 0.05 * salary  # 5% state tax
 print(f"State Tax: ${stateTax:.2f}")
 
 # Calculate federal tax here
-federalTax = 0.10 * salary
-
+federalTax = 0.10 * salary  # 10% federal tax
 print(f"Federal Tax: ${federalTax:.2f}")
 
 # Calculate dependent deduction here
-dependentDeduction = 100 * numDependents
-
+dependentDeduction = 100 * numDependents  # $100 per dependent
 print(f"Dependents Deduction: ${dependentDeduction:.2f}")
 
+# Calculate take-home pay here
+takeHomePay = salary - stateTax - federalTax + dependentDeduction
 
-# Calculate take home pay here.
-
+# Output the final results
 print(f"Salary: ${salary:.2f}")
 print(f"Take-Home Pay: ${takeHomePay:.2f}")
