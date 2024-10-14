5772f86904968aba719e2ec06e637cb646d41d98
tahasalayma <taha.salayma@gmail.com>
2024-10-14 19:59:05 +0000

    Testing Mon Oct 14 2024 19:59:05 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex02/student/Furniture.py
+++ chapter2/ex02/student/Furniture.py
@@ -4,8 +4,9 @@ item_name = "TV Stand"
 retail_price = 325.00
 wholesale_price = 200.00
 # Write your assignment statements here.
-
-
+profit = retail_price - wholesale_price
+sale_price = retail_price * 0.75
+sale_profit = sale_price - wholesale_price
 print(f"Item Name: {item_name}")
 print(f"Retail Price: ${retail_price:.2f}")
 print(f"Wholesale Price: ${wholesale_price:.2f}")
