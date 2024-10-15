9f9d4f8de52d0734109e9115dc500cc503312e57
tahasalayma <taha.salayma@gmail.com>
2024-10-15 18:14:10 +0000

    Testing Tue Oct 15 2024 18:14:10 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex03/student/EmployeeBonus.py
+++ chapter4/ex03/student/EmployeeBonus.py
@@ -7,15 +7,17 @@ transaction_value = float(input("Transaction dollar value: "))
 # Task 1: Calculate productivity score
 productivity_score = (transaction_value / num_transactions) / num_shifts
 
-# Task 2: Determine the bonus using a nested if statement
+# Task 2: Determine the bonus using nested if statements
 if productivity_score <= 30:
     bonus = 50
-elif 31 <= productivity_score <= 69:
-    bonus = 75
-elif 70 <= productivity_score <= 199:
-    bonus = 100
 else:
-    bonus = 200
+    if productivity_score <= 69:
+        bonus = 75
+    else:
+        if productivity_score <= 199:
+            bonus = 100
+        else:
+            bonus = 200
 
 # Output (pre-written)
 print(f"Employee Name: {employee_name}")
