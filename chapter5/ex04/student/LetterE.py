3f7369e09ee18f4df418ea45d41c386e6f428d4b
tahasalayma <taha.salayma@gmail.com>
2024-10-15 21:25:35 +0000

    Testing Tue Oct 15 2024 21:25:35 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex04/student/LetterE.py
+++ chapter5/ex04/student/LetterE.py
@@ -4,24 +4,25 @@
 # Output: Prints the letter E.
 
 num_across = 3  # Number of asterisks to print across.
-num_down = 5  # Number of asterisks to print down.
+num_down = 5    # Number of asterisks to print down.
 
-# Write a loop to control the number of rows.
+# Loop through each row
+for i in range(num_down):
+    # Loop through each column
+    for j in range(num_across):
+        # Print an asterisk in the first column
+        if j == 0:
+            print("*", end="")
+        # Print asterisks in the first, third, and last rows
+        elif (i == 0 or i == 2 or i == 4) and j < num_across:
+            print("*", end="")
+        # Print a space in the second and fourth rows for the middle column
+        elif (i == 1 or i == 3) and j == 1:
+            print(" ", end="")
+        # Print an asterisk in the last column for second and fourth rows
+        elif (i == 1 or i == 3) and j == 2:
+            print("*", end="")
+        else:
+            print(" ", end="")  # Print space for any other case
 
-# Write a loop to control the number of columns
-
-# Decide when to print an asterisk in every column.
-
-print("*", end="")
-
-# Decide when to print asterisk in column 1.
-
-print("*", end="")
-
-# Decide when to print a space instead of an asterisk.
-
-print(" ", end="")
-
-# Figure out where to place this statement that prints a newline.
-
-print()
+    print()  # Move to the next line after finishing a row
