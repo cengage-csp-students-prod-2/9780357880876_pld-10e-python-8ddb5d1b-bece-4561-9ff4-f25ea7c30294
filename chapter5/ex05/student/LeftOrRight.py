c573919f254fbf5d09add28710e5b1d8e96d3d49
tahasalayma <taha.salayma@gmail.com>
2024-10-15 21:37:27 +0000

    Testing Tue Oct 15 2024 21:37:27 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex05/student/LeftOrRight.py
+++ chapter5/ex05/student/LeftOrRight.py
@@ -1,14 +1,24 @@
 # LeftOrRight.py - This program calculates the total number of left-handed and right-handed
-#                    students in a class.
-# Input:  L for left-handed; R for right handed; X to quit.
+# students in a class.
+# Input:  L for left-handed; R for right-handed; X to quit.
 # Output: Prints the number of left-handed students and the number of right-handed students.
 
 rightTotal = 0  # Number of right-handed students.
-leftTotal = 0  # Number of left-handed students.
+leftTotal = 0   # Number of left-handed students.
 
-leftOrRight = input("Enter L if you are left-handed, R if you are right-handed or X to quit: ")
+# Start a loop that continues until the user enters 'X'
+while True:
+    leftOrRight = input("Enter L if you are left-handed, R if you are right-handed or X to quit: ").strip().upper()
 
-# Write your loop here.
+    # Check the user input
+    if leftOrRight == 'L':
+        leftTotal += 1  # Increment left-handed counter
+    elif leftOrRight == 'R':
+        rightTotal += 1  # Increment right-handed counter
+    elif leftOrRight == 'X':
+        break  # Exit the loop if the user enters 'X'
+    else:
+        print("Invalid input. Please enter L, R, or X.")
 
 # Output number of left or right-handed students.
 print("Number of left-handed students:", leftTotal)
