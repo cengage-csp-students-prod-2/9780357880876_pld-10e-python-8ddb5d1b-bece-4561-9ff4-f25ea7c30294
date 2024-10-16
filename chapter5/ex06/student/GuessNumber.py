40b6d81127ba2a650245ea87f24edf094db863f3
tahasalayma <taha.salayma@gmail.com>
2024-10-16 16:29:13 +0000

    Testing Wed Oct 16 2024 16:29:13 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex06/student/GuessNumber.py
+++ chapter5/ex06/student/GuessNumber.py
@@ -1,46 +1,18 @@
-from random import randint
+mport random
 
-def guessing_game():
-    number = randint(1, 10)  # Generate random number between 1 and 10.
-
-    # Prime the loop.
-    keepGoing = input("Do you want to guess a number? Enter Y or N: ").strip().upper()
-
-    # Validate input for 'Y' or 'N'
-    while keepGoing not in ('Y', 'N'):
-        keepGoing = input("Invalid input. Please enter Y or N: ").strip().upper()
-
-    # Enter loop if they want to play.
-    while keepGoing == "Y":
-        # Get user's guess and validate input.
-        while True:
-            try:
-                stringNumber = input("I'm thinking of a number...\nTry to guess by entering a number between 1 and 10: ")
-                userNumber = int(stringNumber)
-
-                # Validate if the number is in range
-                if 1 <= userNumber <= 10:
-                    break
-                else:
-                    print("Number must be in the range of 1 to 10. Please try again.")
-            except ValueError:
-                print("Invalid input. Please enter a valid integer.")
-
-        # Test to see if the user guessed correctly.
-        if userNumber == number:
-            print("You are a genius. That's correct!")
-            break  # Exit the loop if correct guess is made.
-
+while True:
+    try:
+        number_in = int(input("Please insert a number between 1-10: "))
+        if 1 <= number_in <= 10:
+            break
         else:
-            # Ask if the user wants to guess again.
-            keepGoing = input("That's not correct. Do you want to guess again? Enter Y or N: ").strip().upper()
+            print("Your input was invalid. Please insert a number from 1 to 10")
+    except ValueError:
+        print("Sorry. Try again to insert a number from 1 to 10")
 
-            # Validate input for 'Y' or 'N'
-            while keepGoing not in ('Y', 'N'):
-                keepGoing = input("Invalid input. Please enter Y or N: ").strip().upper()
+game_1 = random.randint(1, 10)
 
-    # End of the game.
-    print("Thank you for playing!")
-
-if __name__ == "__main__":
-    guessing_game()
+if number_in == game_1:
+    print(f"The result was {game_1}. Your guess was correct, congratulations!")
+else:
+    print(f"The result was {game_1}. Your guess was NOT correct. Try again")
