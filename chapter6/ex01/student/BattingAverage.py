adcccaca6bca602db29e0d5dfb9c8f8f5399daec
tahasalayma <taha.salayma@gmail.com>
2024-10-16 20:03:06 +0000

    Testing Wed Oct 16 2024 20:03:06 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex01/student/BattingAverage.py
+++ chapter6/ex01/student/BattingAverage.py
@@ -1,19 +1,19 @@
 # Initialize an integer for list size here.
-size = 8
+MAX_AVERAGES = 8
 
 # Initialize list here.
-averages = [0] * size
+averages = []
 
-# Write a loop to get batting averages from user and add to list.
-for i in range(size):
+# Write a loop to get batting averages from user and assign to list.
+for loopIndex in range(MAX_AVERAGES):
     # String version of batting average input by user.
     averageString = input("Enter a batting average: ")
-    
+
     # Use this variable to store the batting average input by user.
     battingAverage = float(averageString)
-    
-    # Add value to list.
-    averages[i] = battingAverage
+
+    # Assign value to list.
+    averages.append(battingAverage)
 
 # Use these variables to store the minimum and maximum batting averages.
 # Assign the first element in the list to be the minimum and the maximum.
@@ -23,25 +23,25 @@ max_average = averages[0]
 # Start out your total initialized to 0.
 total = 0
 
-# Write a loop here to access list values starting with averages[0]
-for i in range(size):
+# Write a loop here to access list values and calculate total, min, and max
+for average in averages:
+    # Accumulate a total of all batting averages.
+    total += average
+
     # Within the loop, test for minimum and maximum batting averages.
-    if averages[i] < min_average:
-        min_average = averages[i]
-    if averages[i] > max_average:
-        max_average = averages[i]
-    
-    # Also accumulate a total of all batting averages.
-    total += averages[i]
+    if average < min_average:
+        min_average = average
+    if average > max_average:
+        max_average = average
 
 # Calculate the average of the 8 averages.
-average_of_averages = total / size
+average_of_averages = total / MAX_AVERAGES
 
 # Print the averages stored in the averages list.
-for i in range(size):
-    print(f"averages[{i}] is: {averages[i]}")
+for index in range(len(averages)):
+    print(f"averages[{index}] is: {averages[index]}")
 
-# Print the maximum batting average, minimum batting average, and average batting average.
+# Print the minimum batting average, maximum batting average, and average batting average.
 print(f"Minimum batting average is {min_average:.3f}")
 print(f"Maximum batting average is {max_average:.3f}")
-print(f"Average batting average is {average_of_averages:.3f}")
+print(f"Average batting average is {average_of_averages:.4f}")
