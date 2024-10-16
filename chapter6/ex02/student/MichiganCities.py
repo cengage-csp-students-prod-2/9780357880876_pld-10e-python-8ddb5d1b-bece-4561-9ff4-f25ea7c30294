3a984897d2ab9133f90164d2ae40cda710b7336f
tahasalayma <taha.salayma@gmail.com>
2024-10-16 19:28:20 +0000

    Testing Wed Oct 16 2024 19:28:20 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex02/student/MichiganCities.py
+++ chapter6/ex02/student/MichiganCities.py
@@ -10,10 +10,17 @@ foundIt = False  # Flag variable.
 # name of city to look up in array.
 inCity = input("Enter name of city: ")
 
-		# Write your loop here.
+# Write your loop here.
+for city in citiesInMichigan:
+    # Write your test statement here to see if there is
+    # a match. Set the flag to true if city is found.
+    if city.lower() == inCity.lower():  # Case-insensitive comparison
+        foundIt = True
+        break  # Exit the loop since we found a match
 
-			# Write your test statement here to see if there is
-			# a match. Set the flag to true if city is found.
-
-		# Test to see if city was not found to determine if
-		# "Not a city in Michigan." message should be printed.
+# Test to see if city was not found to determine if
+# "Not a city in Michigan." message should be printed.
+if foundIt:
+    print("City found.")
+else:
+    print("Not a city in Michigan.")
