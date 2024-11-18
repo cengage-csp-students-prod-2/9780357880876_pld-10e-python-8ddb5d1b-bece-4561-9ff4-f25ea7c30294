437d1f46ee9678ff3cfd191b5b27a85a255c8d7c
tahasalayma <taha.salayma@gmail.com>
2024-11-18 21:46:44 +0000

    Testing Mon Nov 18 2024 21:46:43 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex02/student/HouseholdSize.py
+++ chapter8/ex02/student/HouseholdSize.py
@@ -4,8 +4,8 @@
 # Output: Mean and median household size.
 
 # Initialize variables.
-SIZE = 300 # Maximum number of household sizes.
-householdSizes = [] # List used to store up to 300 household sizes.
+SIZE = 300  # Maximum number of household sizes.
+householdSizes = []  # List used to store up to 300 household sizes.
 householdSize = 0
 total = 0
 mean = 0
@@ -20,34 +20,40 @@ x = 0
 while (x < SIZE) and (householdSize != 999):
 
     # Place value in list.
-    householdSizes[x] = householdSize
+    householdSizes.append(householdSize)
 
     # Calculate total of household sizes
-
+    total += householdSize
 
     # Get ready for next input item.
     x += 1
-
     householdSizeString = input("Enter household size or 999 to quit: ")
     householdSize = int(householdSizeString)
-    # End of input loop.
+# End of input loop.
 
-# new size of households
-
+# New size of households
+size = len(householdSizes)
 
 # Find the mean
-
+if size > 0:
+    mean = total / size
 
 # This is the work done in the sortList() method
-# Number of items to compare.
-
-# Set flag to False.
-
-# Outer loop controls number of passes over data.
-# Test flag.
-
-# This is the work done in the displayList() method
-
+# Bubble sort in descending order
+for i in range(size):
+    for j in range(0, size - i - 1):
+        if householdSizes[j] < householdSizes[j + 1]:
+            # Swap elements
+            householdSizes[j], householdSizes[j + 1] = householdSizes[j + 1], householdSizes[j]
 
 # Calculate the median depending on size of list
-# Print the median
+if size > 0:
+    if size % 2 == 0:  # Even number of elements
+        median = (householdSizes[size // 2 - 1] + householdSizes[size // 2]) / 2
+    else:  # Odd number of elements
+        median = householdSizes[size // 2]
+
+# This is the work done in the displayList() method
+# Print the results
+print(f"Mean household size in Marengo is: {mean}")
+print(f"Median household size in Marengo is: {median}")
