4f5c15035a8444d1d3e8a8bdf54da87d4ddd728b
tahasalayma <taha.salayma@gmail.com>
2024-12-02 20:38:56 +0000

    Testing Mon Dec 02 2024 20:38:56 GMT+0000 (Coordinated Universal Time)

--- chapter9/ex01/student/ArtShow.py
+++ chapter9/ex01/student/ArtShow.py
@@ -4,18 +4,22 @@
 # Output:  A statement telling the user if they get a discount or no discount.
 
 def main():
+    registerString = input("Did you preregister? Enter Y or N: ")
 
-		registerString = input("Did you preregister? Enter Y or N: ")
-
-		# Test input here. If Y, call discount(), else call noDiscount().
-
-# End of main() function.
-
+    # Test input here. If Y, call discount(), else call noDiscount().
+    if registerString.upper() == 'Y':
+        discount()  # Call discount() if user has preregistered.
+    else:
+        noDiscount()  # Call noDiscount() if user hasn't preregistered.
 
 # Write discount function here.
+def discount():
+    print("You are pre-registered and qualify for a 5% discount.")
 
 # Write noDiscount function here.
-
+def noDiscount():
+    print("Sorry, you did not pre-register and do not qualify for a 5% discount.")
 
 if __name__ == '__main__':
-# Call the main function to run program
+    # Call the main function to run program
+    main()
