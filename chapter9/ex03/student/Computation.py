1e3e6686d479587347f5f7da1f2db7a89ff4e681
tahasalayma <taha.salayma@gmail.com>
2024-12-04 20:15:43 +0000

    Testing Wed Dec 04 2024 20:15:42 GMT+0000 (Coordinated Universal Time)

--- chapter9/ex03/student/Computation.py
+++ chapter9/ex03/student/Computation.py
@@ -3,29 +3,37 @@
 # Output:  Sum, difference, and product of two values.
 
 def main():
-
     value1String = input("Enter first numeric value: ")
     value1 = float(value1String)
     value2String = input("Enter second numeric value: ")
     value2 = float(value2String)
 
-
     # Call calculateSum() here
+    calculateSum(value1, value2)
 
     # Call calculateDifference() here
+    calculateDifference(value1, value2)
 
     # Call calculateProduct() here
+    calculateProduct(value1, value2)
 
 # End of main() function.
 
 # Write calculateSum() function here.
-
+def calculateSum(num1, num2):
+    sum_result = num1 + num2
+    print(f"Sum is: {sum_result}")
 
 # Write calculateDifference() function here.
-
+def calculateDifference(num1, num2):
+    difference_result = num1 - num2
+    print(f"Difference is: {difference_result}")
 
 # Write calculateProduct() function here.
-
+def calculateProduct(num1, num2):
+    product_result = num1 * num2
+    print(f"Product is: {product_result}")
 
 if __name__ == '__main__':
-# Call the main function to run program
+    # Call the main function to run program
+    main()
